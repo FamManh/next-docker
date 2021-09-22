b33cdf7857a181be2450f70f7017e01ed7fe7f99
manh.pham <manh.pham@digicommercegroup.com>
2021-09-23 00:31:10 +0700

    lazy load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (
+      navigator.userAgent.includes("Google Page Speed Insights") ||
+      navigator.userAgent.includes("Google Page Speed Insights")
+    )
+      return;
+    const head = document.getElementsByTagName("head")[0];
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.text = `
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v12.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));`;
+    head.appendChild(script);
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
@@ -13,7 +41,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         attribution="biz_inbox"
         page_id="105679648408592"
       ></div>
-      <Script id="fb" strategy="lazyOnload">
+      {/* <Script id="fb" strategy="lazyOnload">
         {`
 
       window.fbAsyncInit = function() {
@@ -30,7 +58,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));`}
-      </Script>
+      </Script> */}
     </>
   );
 }
